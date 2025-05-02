1aca443be565054b37699c18b7f29fec86c40186
unknown <k.moksud@gmail.com>
2025-05-02 14:10:38 +0600

    initial updates

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from sqlalchemy import create_engine
-# from sqlalchemy.pool import NullPool
-from dotenv import load_dotenv
-import os
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-#from app.api.v1.endpoints import products, sales, auth, inventory_logs, cash_register, transaction, general_ledger,cash_flow, enum_type, account, journal_entry, vendor, purchase_orders
 app = FastAPI()
 
 @app.get("/")
